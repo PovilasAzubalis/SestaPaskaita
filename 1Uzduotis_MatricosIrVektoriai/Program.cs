8b7382b4dacb43ff41ebc38173ff22d1324b204c
azubalispovilas <azubalispovilas@gmail.com>
2024-05-31 10:32:03 +0300

    .

--- 1Uzduotis_MatricosIrVektoriai/Program.cs
+++ 1Uzduotis_MatricosIrVektoriai/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Sukurkite konsolinę programą, kuri leis vartotojui įvesti n filmų pavadinimus ir jų reitingus (nuo 1 iki 10).
+// Duomenis saugokite sąraše ar masyve.
+
+// Leiskite vartotojui pasirinkti iš meniu, ką jis nori atlikti su įvestais duomenimis:
+// a.Rodyti visus filmus ir jų reitingus.
+// b. Rodyti tik tuos filmus, kurių reitingas didesnis nei nurodyta vertė.
+// c. Rasti filmą pagal pavadinimą ir parodyti jo reitingą.
+// d. Atnaujinti filmo reitingą.
+// e. Ištrinti filmą iš sąrašo.
+// f. Išeiti iš programos.
